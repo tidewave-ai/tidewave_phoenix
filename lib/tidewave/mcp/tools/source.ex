b8c2bbc56ab59c77313716b5f5ce59b71fe04f64
Steffen Deusch <steffen@deusch.me>
2025-05-06 17:29:47 +0200

    remove unnecessary code

--- lib/tidewave/mcp/tools/source.ex
+++ lib/tidewave/mcp/tools/source.ex
@@ -179,14 +179,8 @@ defmodule Tidewave.MCP.Tools.Source do
 
   defp decompose({:/, _, [call, arity]}, context) do
     case Macro.decompose_call(call) do
-      {_mod, :__info__, []} when arity == 1 ->
-        {Module, :__info__, 1}
-
       {mod, fun, []} ->
         {Macro.expand(mod, context), fun, arity}
-
-      {fun, []} ->
-        {find_decompose_fun_arity(fun, arity, context), fun, arity}
 
       _ ->
         :error
@@ -195,64 +189,11 @@ defmodule Tidewave.MCP.Tools.Source do
 
   defp decompose(call, context) do
     case Macro.decompose_call(call) do
-      {_mod, :__info__, []} ->
-        {Module, :__info__, 1}
-
-      {maybe_sigil, [_, _]} ->
-        case Atom.to_string(maybe_sigil) do
-          "sigil_" <> _ ->
-            {find_decompose_fun_arity(maybe_sigil, 2, context), maybe_sigil, 2}
-
-          _ ->
-            :error
-        end
-
       {mod, fun, []} ->
         {Macro.expand(mod, context), fun}
-
-      {fun, []} ->
-        {find_decompose_fun(fun, context), fun}
 
       _ ->
         :error
     end
-  end
-
-  defp find_decompose_fun(fun, context) do
-    find_import(fun, context.functions) || find_import(fun, context.macros) ||
-      find_special_form(fun) || Kernel
-  end
-
-  defp find_decompose_fun_arity(fun, arity, context) do
-    pair = {fun, arity}
-
-    find_import(pair, context.functions) || find_import(pair, context.macros) ||
-      find_special_form(pair) || Kernel
-  end
-
-  defp find_import(pair, context) when is_tuple(pair) do
-    Enum.find_value(context, fn {mod, functions} ->
-      if pair in functions, do: mod
-    end)
-  end
-
-  defp find_import(fun, context) do
-    Enum.find_value(context, fn {mod, functions} ->
-      if Keyword.has_key?(functions, fun), do: mod
-    end)
-  end
-
-  defp find_special_form(pair) when is_tuple(pair) do
-    special_form_function? = pair in Kernel.SpecialForms.__info__(:functions)
-    special_form_macro? = pair in Kernel.SpecialForms.__info__(:macros)
-
-    if special_form_function? or special_form_macro?, do: Kernel.SpecialForms
-  end
-
-  defp find_special_form(fun) do
-    special_form_function? = Keyword.has_key?(Kernel.SpecialForms.__info__(:functions), fun)
-    special_form_macro? = Keyword.has_key?(Kernel.SpecialForms.__info__(:macros), fun)
-
-    if special_form_function? or special_form_macro?, do: Kernel.SpecialForms
   end
 end
