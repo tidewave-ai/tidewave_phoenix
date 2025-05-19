137f1c1b608dcb031761d0640f49c10b6e4a7c52
Zach Daniel <zach@zachdaniel.dev>
2025-05-19 17:32:08 -0400

    update tests for recent changes

--- test/mcp/tools_test.exs
+++ test/mcp/tools_test.exs
@@ -12,17 +12,19 @@ defmodule Tidewave.MCP.ToolsTest do
 
   test "tools are hidden from the list when excluded" do
     refute Enum.any?(
-             Tidewave.MCP.Server.tools({%{}, [tools: [exclude: ["project_eval"]]]}),
+             Tidewave.MCP.Server.tools({%{}, %{exclude_tools: ["project_eval"]}}),
              &(&1.name == "project_eval")
            )
 
     assert Enum.any?(
-             Tidewave.MCP.Server.tools({%{}, [tools: [exclude: []]]}),
+             Tidewave.MCP.Server.tools({%{}, %{exclude_tools: []}}),
              &(&1.name == "project_eval")
            )
 
     assert Enum.map(
-             Tidewave.MCP.Server.tools({%{}, [tools: [include: ["project_eval"]]]}),
+             Tidewave.MCP.Server.tools(
+               {%{}, %{include_tools: ["project_eval"], exclude_tools: []}}
+             ),
              & &1.name
            ) == ["project_eval"]
   end
