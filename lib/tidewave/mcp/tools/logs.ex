5f5c58cc5d57028943b3e5acc779fca00df72f79
José Valim <jose.valim@gmail.com>
2025-09-08 17:47:25 +0200

    Update lib/tidewave/mcp/tools/logs.ex
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>

--- lib/tidewave/mcp/tools/logs.ex
+++ lib/tidewave/mcp/tools/logs.ex
@@ -21,7 +21,7 @@ defmodule Tidewave.MCP.Tools.Logs do
             grep: %{
               type: "string",
               description:
-                "Filter logs with the given regular expression (case insensitive). E.g. \"error\" when you to capture errors in particular"
+                "Filter logs with the given regular expression (case insensitive). E.g. \"error\" when you want to capture errors in particular"
             }
           }
         },
